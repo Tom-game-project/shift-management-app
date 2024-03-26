5c170718315f7f38cf712235f3da5dd4b65889ec
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-03-26 20:13:41 +0900

    add MatchingCls

--- gas/code.ts
+++ gas/code.ts
@@ -1,11 +1,46 @@
 // calendar 
 
-function getCalendar(){
-  // カレンダーを取得する
-  const calendarId = PropertiesService.getScriptProperties().getProperty("calendarID");
-  let calendar = CalendarApp.getCalendarById(calendarId!);
-  console.log(calendar.getName());
+class MatchingCls{
+
+  private worksSheet:GoogleAppsScript.Spreadsheet.Sheet;
+  private staffSheet:GoogleAppsScript.Spreadsheet.Sheet;
+  private calendar  :GoogleAppsScript.Calendar.Calendar;
+
+  constructor (){
+    // get id
+    const spreadsheetId = PropertiesService.getScriptProperties().getProperty("spreadsheetID");
+    const calendarId = PropertiesService.getScriptProperties().getProperty("calendarID");
+    // get calendar
+    this.calendar = CalendarApp.getCalendarById(calendarId!);
+    // spreadSheet
+    let spreadSheet = SpreadsheetApp.openById(spreadsheetId!);
+    this.worksSheet = spreadSheet.getSheetByName("works")!;
+    this.staffSheet = spreadSheet.getSheetByName("staff")!;
+  }
+
+  getWorks(){
+    let rlist:Array<String> = [];
+    let range = this.worksSheet.getDataRange();
+    for (const work of range.getValues()){
+      rlist.push(work[0]);
+    }
+    return rlist;
+  }
+
+  getStaff(){
+    // get all rows
+    let range = this.staffSheet.getDataRange();
+    return range.getValues();
+  }
 }
 
 
+/**
+ * # test00
+ */
+function test00(){
+  const mc = new MatchingCls();
+  console.log(mc.getWorks());
+  console.log(mc.getStaff());
+}
 
